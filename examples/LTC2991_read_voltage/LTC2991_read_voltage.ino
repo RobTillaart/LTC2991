746e16332855e3ccc2a4a7b4873e6e7a161ce925
Rob Tillaart <rob.tillaart@gmail.com>
2023-01-23 13:54:18 +0100

    clean up code (#8)
    - update GitHub actions
    - update license 2023
    - update readme.md
    - update examples
    - fix header unit test
    - minor edits
    - move code to .cpp

--- examples/LTC2991_read_voltage/LTC2991_read_voltage.ino
+++ examples/LTC2991_read_voltage/LTC2991_read_voltage.ino
@@ -1,7 +1,6 @@
 //
 //    FILE: LTC2991_read_voltage.ino
 //  AUTHOR: Rob Tillaart
-//    DATE: 2021-05-13
 // PUPROSE: demo
 
 
@@ -28,13 +27,13 @@ void setup()
 
   for (uint8_t channel = 1; channel <= 4; channel++)
   {
-    // enable all channels  ==> are enabled in pairs! )
+    //  enable all channels  ==> are enabled in pairs! )
     LTC.enable(channel, true);
     LTC.enable_filter(channel, false);
     LTC.set_mode_voltage_normal(channel);
   }
 
-  // to get multiple readings in loop()
+  //  to get multiple readings in loop()
   LTC.set_acquisition_repeat();
 }
 
@@ -57,4 +56,4 @@ void loop()
 }
 
 
-// -- END OF FILE --
+//  -- END OF FILE --
